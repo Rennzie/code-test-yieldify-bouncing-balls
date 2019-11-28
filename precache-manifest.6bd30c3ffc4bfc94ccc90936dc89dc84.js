31cf3b82996a3ececa293954d275df42d85dd28c
Sean.Rennie <sean.rennie6@gmail.com>
2019-11-28 09:38:26 +0200

    Updates

---
+++ precache-manifest.6bd30c3ffc4bfc94ccc90936dc89dc84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb463f36f8e7f09163ca7e55e25c4065",
+    "url": "/code-test-yieldify-bouncing-balls/index.html"
+  },
+  {
+    "revision": "f867eb67b2a94fa6bdb5",
+    "url": "/code-test-yieldify-bouncing-balls/static/css/main.96dce5da.chunk.css"
+  },
+  {
+    "revision": "7adaab43aa079bc4692b",
+    "url": "/code-test-yieldify-bouncing-balls/static/js/2.90e59565.chunk.js"
+  },
+  {
+    "revision": "f867eb67b2a94fa6bdb5",
+    "url": "/code-test-yieldify-bouncing-balls/static/js/main.cad5b1db.chunk.js"
+  },
+  {
+    "revision": "550de48792dad2778681",
+    "url": "/code-test-yieldify-bouncing-balls/static/js/runtime-main.1ab44336.js"
+  }
+]);
